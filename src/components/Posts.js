d468fe1ea49f23d77955a1f5040d33d5fe5b86bb
santosjack <santosjack@pop-os.localdomain>
2023-01-06 12:02:34 -0300

    feat: adjustment of posts listing

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,8 +1,50 @@
-
+import styled from "styled-components";
+import { useState, useContext, useEffect } from "react";
+import { contexto } from "../context/userContext";
+import Post from "./Post";
+import { getTimeline } from "../service/api";
 
 export default function Posts(){
+    const [loading, setLoading] = useState(false);
+    const [posts, setPosts] = useState([]);
+    const { token } = useContext(contexto);
+
+    useEffect(() => {
+        getTimeline(token.token)
+        .then((res)=> {
+            setPosts(res.data)
+        })
+        .catch()
+    }, []);
+
+
     return(
         <>
+            <PostsContainer>
+                {posts.length > 0 ? (
+                    posts.map(p => <Post key={p.id} data={p}/>)
+                ): (
+                    <div>loading...</div>
+                )}
+            </PostsContainer>
         </>
     )
 }
+
+const PostsContainer = styled.div`
+    width: 611px;
+    margin-top: 29px;
+
+    :last-child{
+        margin-bottom: 50px;
+    }
+
+    @media screen and (max-width: 600px) {
+        max-width: 100vw;
+
+    }
+
+    @media screen and (max-width: 768px) {
+        max-width: 100vw;
+    }
+`
